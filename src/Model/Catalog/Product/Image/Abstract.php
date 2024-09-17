93014ac0a7c5fb473266dec6c5790197026fceae
mklooss <mathis@mage-profis.de>
2024-09-17 09:51:28 +0200

    added watermark

--- src/Model/Catalog/Product/Image/Abstract.php
+++ src/Model/Catalog/Product/Image/Abstract.php
@@ -5,6 +5,11 @@ if ((string)Mage::getConfig()->getModuleConfig('Netzarbeiter_NicerImageNames')->
     class MageProfis_ImageQueue_Model_Catalog_Product_Image_Abstract
     extends Netzarbeiter_NicerImageNames_Model_Image
     { }
+} elseif ((string)Mage::getConfig()->getModuleConfig('FireGento_PerfectWatermarks')->active == 'true')
+{
+    class MageProfis_ImageQueue_Model_Catalog_Product_Image_Abstract
+    extends FireGento_PerfectWatermarks_Model_Product_Image
+    { }
 } else {
     class MageProfis_ImageQueue_Model_Catalog_Product_Image_Abstract
     extends Mage_Catalog_Model_Product_Image
